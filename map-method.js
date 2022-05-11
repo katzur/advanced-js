837b530ed46081b1f0bd3b6b579703240be751c1
katzur <zurawska.kasia@gmail.com>
2022-05-11 18:51:44 +0000

    Add map method example

---
+++ map-method.js
@@ -0,0 +1,52 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node map.js in the terminal
+ */
+
+// Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let result = [];
+for (let num of nums) {
+    result.push(num * 2);
+}
+console.log(result);
+
+// Using map()
+const multiplyByTwo = function (num) {
+    return num * 2;
+}
+const mapResult = nums.map(multiplyByTwo);
+console.log(mapResult);
+
+// Simplified w/ map()
+const simplified = nums.map(function(num) {return num * 2});
+console.log(simplified);
+
+// Simplfied w/ map() + arrow function
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
+
+// With objects:
+const students = [
+    {
+      id: 1,
+      name: 'Mark',
+      profession: 'Developer',
+      skill: 'JavaScript'
+    },
+    {
+      id: 2,
+      name: 'Ariel',
+      profession: 'Developer',
+      skill: 'HTML'
+    },
+    {
+      id: 3,
+      name: 'Jason',
+      profession: 'Designer',
+      skill: 'CSS'
+    },
+  ];
+
+const studentsWithIds = students.map(student => [student.name, student.id] );
+console.log(studentsWithIds)
